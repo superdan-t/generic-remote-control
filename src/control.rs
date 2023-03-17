4925cf320537bb7910e2d3f52be0826f9de7bf89
Daniel Tierney <daniel@dtierney.dev>
2023-03-16 22:50:16 -0400

    Add input type struct with reworked identifiers

--- src/control.rs
+++ src/control.rs
@@ -1,60 +1,57 @@
 //! Contains interfaces for input and output controls for remote devices.
 
-use std::string::String;
-
-/// An identifier is used to name, identify, and compare specific inputs and outputs.
-pub struct Identifier {
-    /// The primary name is used internally for all comparisons between IDs
-    name: String,
-
-    /// The display name is meant to be a user-friendly name for UIs to show, but has no functional impact on the ID
-    display_name: String,
-
-    /// This can provide information about the input/output this ID represents that UIs may choose to show, but has no functional impact on the ID
-    description: Option<String>,
-}
-
-impl Identifier {
-    pub fn new(name: &str, display_name: &str, description: Option<&str>) -> Identifier {
-        Identifier {
-            name: String::from(name),
-            display_name: String::from(display_name),
-            description: description.map(str::to_string),
-        }
-    }
-
-    pub fn get_display_name(&self) -> &str {
-        &self.display_name
-    }
-
-    pub fn get_name(&self) -> &str {
-        &self.name
-    }
-
-    pub fn get_description(&self) -> Option<&str> {
-        self.description.as_deref()
-    }
-}
+use std::any::TypeId;
 
 /// Represents a type of value provided as an input to control a remote device.
-pub struct Input {
-    pub id: Identifier,
+pub trait InputType {
+
+    /// Get the name and other unique info for this input type
+    fn get_id(&self) -> &Identifier;
+
+    /// Get the types of parameters that this input type expects
+    fn get_parameters(&self) -> [TypeId];
 }
 
+/// An identifier is used to name, identify, and compare specific inputs and outputs.
+pub struct Identifier<'a> {
+    /// The primary name is used internally for all comparisons between IDs
+    pub name: &'a str,
+    /// The display name is meant to be a user-friendly name for UIs to show, but has no functional impact on the ID
+    pub display_name: &'a str,
+    /// This can provide information about the input/output this ID represents that UIs may choose to show, but has no functional impact on the ID
+    pub description: Option<&'a str>,
+}
+
+
 #[cfg(test)]
+#[allow(unused_variables)]
 mod tests {
-    use super::Identifier;
+    use std::{any::TypeId, collections::BTreeMap};
 
-    /// Shows creating an identifier with string slices and tests that the getters match what it was constructed with
+    use super::{Identifier};
+
+    struct BasicInputType {
+        id: Identifier<'static>,
+        parameters: Vec<TypeId>,
+    }
+
+    // Creates a map of inputs; mostly a compile-type design check
     #[test]
-    fn create_identifier() {
-        let name = "example.tree.endpoint";
-        let display_name = "Endpoint";
-        let description = "This is an example of an ID that can describe a control object";
-        let id = Identifier::new(name, display_name, Some(description));
+    fn collection_of_inputs() {
+        let send_coordinates = BasicInputType {
+            id: Identifier { name: "control.coordinates", display_name: "Coordinates", description: None },
+            parameters: vec![TypeId::of::<f32>(); 2],
+        };
+        let send_speed = BasicInputType {
+            id: Identifier { name: "control.speed", display_name: "Forward Movement Speed", description: None },
+            parameters: vec![TypeId::of::<f32>(); 1],
+        };
 
-        assert_eq!(name, id.get_name());
-        assert_eq!(display_name, id.get_display_name());
-        assert_eq!(description, id.get_description().unwrap());
+        let mut inputs = BTreeMap::new();
+
+        inputs.insert(send_coordinates.id.name, send_coordinates);
+        inputs.insert(send_speed.id.name, send_speed);
+
+        inputs.get_key_value("control.speed").unwrap();
     }
 }
